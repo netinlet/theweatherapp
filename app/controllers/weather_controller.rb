e4827dfbedd272ab14fe51c0c57d498ae5d8ae18
Doug Bryant <doug@netinlet.com>
2024-10-29 16:17:00 -0500

    Add in caching and view indicator if request came from cache

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,9 +3,19 @@ class WeatherController < ApplicationController
     @address = params[:address] || "1 Apple Park Way. Cupertino, CA 95014"
     postal_code = @address.match(/\d{5}/)&.to_a&.last
     logger.info "postal_code: #{postal_code}"
+    @cache_hit = false
+    @weather = {}
 
     begin
-      @weather = WeatherService.get_current_weather(postal_code)
+      logger.info "Checking cache for #{postal_code}"
+      if (@weather = Rails.cache.fetch(postal_code))
+        logger.info "Cache hit for #{postal_code}"
+        @cache_hit = true
+      else
+        logger.info "Cache miss for #{postal_code}"
+        @weather = WeatherService.get_current_weather(postal_code)
+        Rails.cache.write(postal_code, @weather, expires_in: 30.minutes, skip_nil: true)
+      end
     rescue => e
       logger.error "Error fetching weather data: #{e.message}"
       flash[:error] = "Error fetching weather data: #{e.message}"
@@ -13,7 +23,7 @@ class WeatherController < ApplicationController
     end
 
     if @weather.empty?
-      flash[:error] = "Please ensure the address is valid and in the following format: 1234 Main St. City, ST 12345"
+      flash[:error] = "Please ensure the address is valid and includes a valid postal code"
     end
 
     render :index
--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe "Weather", type: :request do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "Caching weather data" do
+    it "caches the weather data" do
+      Rails.cache.clear
+      # no cached values...
+      expect(Rails.cache.exist?("95014")).to be_falsey
+
+      get weather_index_path(address: "1 Apple Park Way. Cupertino, CA 95014")
+      expect(Rails.cache.exist?("95014")).to be_truthy # we now have the weather data cached
+      expect(response.body).to include("Cache miss for 95014") # we rendered w/ a cache miss - show that
+
+      # now we'll make sure it's cached
+      get weather_index_path(address: "1 Apple Park Way. Cupertino, CA 95014")
+      expect(Rails.cache.exist?("95014")).to be_truthy
+      expect(response.body).to include("Cache hit for 95014")
+    end
+  end
 end
